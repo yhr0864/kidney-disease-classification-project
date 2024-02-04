43c5bd4d1a9c589eb04b2ba1e058bb8146577ec7
Haoran Yu <yuhaoran0864@gmail.com>
2024-02-04 09:52:14 +0100

    updated version

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # kidney-disease-classification-project
+
+# How to run?
+
+### STEPS:
+
+clone the repo
+
+```bash
+https://github.com/yhr0864/kidney-disease-classification-project.git
+```
+
+### STEP 01- create a conda env after opening the repo
+
+```bash
+conda create -n myenv python=3.9 -y
+```
+
+```bash
+conda activate myenv
+```
+
+### STEP 02- install the requirements
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+#create README interface on github
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0" #project version
+
+REPO_NAME = "kidney-disease-classification-project"
+AUTHOR_USER_NAME = "yhr0864"
+SRC_REPO = "kidney disease classification"
+AUTHOR_EMAIL = "yuhaoran0864@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url={
+        "Bug Tracker": f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "scr"},
+    packages=setuptools.find_packages(where="src")
+)
